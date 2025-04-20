6b9591c4308443c3ae5cf75a0e1b5d4bc462c5da
sharadee Jayarathna <kawya.20233041@iit.ac.lk>
2025-04-20 09:41:10 +0530

    Defined Cart model

--- src/main/java/com/bookstore/model/Cart.java
+++ src/main/java/com/bookstore/model/Cart.java
@@ -7,6 +7,37 @@ public class Cart {
     private int customerid;
     private List<CartItem> items = new ArrayList<>();
 
-    public Cart(){}
-
+    public Cart(){
+        this.items = new ArrayList<>();
+    }
+    public Cart(int customerId){
+        this.customerid = customerId;
+        this.items = new ArrayList<>();
+    }
+    public int getCustomerid() {
+        return customerid;
+    }
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+    public List<CartItem> getItems() {
+        return items;
+    }
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+    public void addItem(CartItem item){
+        this.items.add(item);
+    }
+    public void removeItem(String ISBN){
+        this.items.removeIf(i -> i.getISBN().equals(ISBN));
+    }
+    public void updateItem(String ISBN, int quantity){
+        for(CartItem item : items){
+            if(item.getISBN().equals(ISBN)){
+                item.setQuantity(quantity);
+                return;
+            }
+        }
+    }
 }
